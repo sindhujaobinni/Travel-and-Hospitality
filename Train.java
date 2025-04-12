15321a501ff779965f1ecddb94b2f2dea46e1973
sindhujaobinni <sindhujaobinni@gmail.com>
2025-04-12 20:56:08 +0530

    Update Train.java

--- Train.java
+++ Train.java
@@ -2,6 +2,8 @@ public class Tickets
   {
     public static void main (String[] args)
     {
+      System.out.println(welcome )
+      System.out.println(select a date)
       System.out.printl(Booked)
     }
   }
